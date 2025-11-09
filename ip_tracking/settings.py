6df4891177d156be83e4e47bc1fc4e3b27d99078
Shirleen Gatu <wambuimunk@gmail.com>
2025-11-09 22:48:56 +0300

    add equirements.txt

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ip_tracking',
-    'ratelimit',
+    'django-ratelimit',
+    'rest_framework',
+    'drf_yasg',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
